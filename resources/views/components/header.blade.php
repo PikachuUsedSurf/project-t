7ada70705c11b0670f33f475f65a3581ad69d5f6
PikachuUsedSurf <nanaosafokissi@gmail.com>
2023-09-19 12:22:53 +0300

    Basically revamped the whole design to make it look like the tx website

--- resources/views/components/header.blade.php
+++
@@ -1,9 +0,0 @@
-<div>
-    <div class=" h-6 bg-emerald-400 flex justify-end">
-        <div class="items-center mr-4">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-        </div>
-    </div>
-</div>
